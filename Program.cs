812df1986c12c4d464cd84716e9856e00e4dd1f5
Taras Nahornyi <systnager@gmail.com>
2025-01-22 14:45:43 +0200

    add Program.cs, .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    private static void Main()
+    { 
+        const int num1 = 5;
+        const int num2 = 8;
+        const int sum = num1 + num2;
+        Console.WriteLine(sum);
+    }
+}
